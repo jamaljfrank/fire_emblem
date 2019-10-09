f7220a8f029b6b814651aa93bd2e900a5b043279
Jamal Frank <jamaljfrank@gmail.com>
2019-10-09 16:26:29 +0000

    Added NOTES.rb

---
+++ NOTES.rb
@@ -0,0 +1,41 @@
+# How to Build a CLI Gem
+# Plan your gem, imagine your interface
+# Start with the project structure - google
+# Start with the entry point - the file run
+# force that to build the CLI interface
+# stub out the interface
+# start making things real
+# discover objects.
+# program
+
+A command line interface for Fire Emblem class information using two pages from serenesforest.net
+
+# user types fire_emblem
+ 
+# shows a list of classes
+# Which class do you want to learn more about?
+# Takes integer on name
+
+# 1. Armored Knight	
+# 2. Cavalier
+# 3. Fighter	
+# 4. Mercenary 	 
+# 5. Monk
+# 6. Myrmidon	 
+# 7. Soldier	
+# 8. Thief
+# 9. ...
+ 
+
+
+# 8
+
+# Thief
+
+# Abilities:
+# “Steal - Allows unit to steal a non-weapon item from a foe with a lower Spd stat.”
+# “Locktouch - “Allows unit to open doors and chests without keys.”
+
+# Mastered Ablility:
+# “Steal - Allows unit to steal a non-weapon item from a foe with a lower Spd stat.”
+# Type list or a class name:
