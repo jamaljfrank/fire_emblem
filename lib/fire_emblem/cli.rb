ba75f63b04ee9d3283beb3d37594dc0c91919027
Jamal Frank <jamaljfrank@gmail.com>
2019-10-09 17:39:09 +0000

    Modified cli

--- lib/fire_emblem/cli.rb
+++ lib/fire_emblem/cli.rb
@@ -2,6 +2,20 @@ class FireEmblem::CLI
   
   def call 
     puts "Select a class:"
+    list
+  end
+  
+  def list 
+    puts <<-DOCS
+    1. Armored Knight	
+    2. Cavalier
+    3. Fighter	
+    4. Mercenary 	 
+    5. Monk
+    6. Myrmidon	 
+    7. Soldier	
+    8. Thief
+    DOCS
   end
 end
 
