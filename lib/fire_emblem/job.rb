eb2f76715f28b5c19b29efba1eb0121a3eb698fd
Jamal Frank <jamaljfrank@gmail.com>
2019-10-12 20:16:10 +0000

    modified job

--- lib/fire_emblem/job.rb
+++ lib/fire_emblem/job.rb
@@ -17,15 +17,16 @@ end
 
 def self.scrape_site
   doc = Nokogiri::HTML(open("https://samurai-gamers.com/fire-emblem-three-houses/archer-class/"))
-  words = ["Fire" , "Emblem:" , "Three" , "Houses" , "-" , "Class"]
+  words = ["Fire" , "Emblem:" , "Three" , "Houses" , "-" , "Class" , "Name" , "Effect" , "\n"]
   re = Regexp.union(words)
+  
   job = {}
   
   job[:name] = doc.search('h1.entry-title').text.split.join.gsub(re, ""),
   
   
-  job[:abilities] = doc.search('//*[@id="post-150519"]/div/table[4]/tbody').text
-  
+  job[:abilities] = doc.search('table')[3].text.gsub(re, "")
+  binding.pry
 
 
   
