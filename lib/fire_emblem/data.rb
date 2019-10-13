eee72a8bd6b42367e678d8199971b9ed71a9ddaa
Jamal Frank <jamaljfrank@gmail.com>
2019-10-13 00:20:07 +0000

    modified job

--- lib/fire_emblem/data.rb
+++ lib/fire_emblem/data.rb
@@ -1,31 +1,23 @@
 class FireEmblem::Data 
-  attr_accessor :name, :abilities
-  
-   @urls = ["https://samurai-gamers.com/fire-emblem-three-houses/archer-class/", "https://samurai-gamers.com/fire-emblem-three-houses/armored-knight-class/", "https://samurai-gamers.com/fire-emblem-three-houses/assassin-class/"]
-  @all_jobs = []
-  
-  def self.initialize
+  def self.initialize(url="https://samurai-gamers.com/fire-emblem-three-houses/archer-class/")
     
-    job = {name: nil, abilities: nil}
-   
-      
-    @urls.each do |url|  
+    
     
     doc = Nokogiri::HTML(open(url))
-  words = ["Fire" , "Emblem:" , "Three" , "Houses" , "-" , "Class" , "Name" , "Effect" , "\n"]
-  re = Regexp.union(words)
+    job = {}
+    words = ["Fire" , "Emblem:" , "Three" , "Houses" , "-" , "Class" , "Name" , "Effect" , "\n"]
+    re = Regexp.union(words)
   
-  job[:name] = doc.search('h1.entry-title').text.split.join.gsub(re, ""),
+    job[:name] = doc.search('h1.entry-title').text.split.join.gsub(re, "")
+    job[:abilities] = doc.search('table')[3].text.gsub(re, "")
+    job
+    binding.pry
   
-  
-  job[:abilities] = doc.search('table')[3].text.gsub(re, ""),
-  @all_jobs << job
-  @all_jobs
   
   
 end
 end
-end
+
 
 
 
--- lib/fire_emblem/job.rb
+++ lib/fire_emblem/job.rb
@@ -1,34 +1,36 @@
 class FireEmblem::Job 
- attr_accessor :name, :abilities, :xability, :xart, :url
- @@jobs = []
+ attr_accessor :name, :abilities, :url
  
  
-def self.scrape
-    
-    
-    @@jobs << self.scrape_site
-    @@jobs
-    
-end
-
 def self.all 
   self.scrape
 end
 
-def self.scrape_site
-  doc = Nokogiri::HTML(open("https://samurai-gamers.com/fire-emblem-three-houses/archer-class/"))
+def self.scrape
+    jobs = []
+    
+    jobs << self.scrape_site
+    jobs
+    
+end
+
+
+
+def self.scrape_site(url = "https://samurai-gamers.com/fire-emblem-three-houses/archer-class/")
+  
+  
+  
+  doc = Nokogiri::HTML(open(url))
   words = ["Fire" , "Emblem:" , "Three" , "Houses" , "-" , "Class" , "Name" , "Effect" , "\n"]
   re = Regexp.union(words)
   
-  job = {}
+  job = self.new
   
-  job[:name] = doc.search('h1.entry-title').text.split.join.gsub(re, "")
-  job[:abilities] = doc.search('table')[3].text.gsub(re, "")
+  job.name = doc.search('h1.entry-title').text.split.join.gsub(re, "")
+  job.abilities = doc.search('table')[3].text.gsub(re, "")
   job
 
 
-  
-  
 end
 end
 
