8885e5aa3bf0e186100e03e24b87aaca0e654010
Jamal Frank <jamaljfrank@gmail.com>
2019-10-13 20:31:10 +0000

    modified data

--- lib/fire_emblem/data.rb
+++ lib/fire_emblem/data.rb
@@ -2,22 +2,17 @@ class FireEmblem::Data
   attr_accessor :name, :abilities, :url, :urls
    
     @@all =[]
-  def self.scrape_site(url="https://samurai-gamers.com/fire-emblem-three-houses/archer-class/")
-  
-  
-
-  
+  def self.scrape_site
+  urls = ["https://samurai-gamers.com/fire-emblem-three-houses/archer-class/" , "https://samurai-gamers.com/fire-emblem-three-houses/armored-knight-class/" , "https://samurai-gamers.com/fire-emblem-three-houses/assassin-class/"]
+  urls.each { |url|
   doc = Nokogiri::HTML(open(url))
   words = ["Fire" , "Emblem:" , "Three" , "Houses" , "-" , "Class" , "Name" , "Effect" , "\n"]
   re = Regexp.union(words)
-  
-  self.new.tap do |job|
-  
-  job.name = doc.search('h1.entry-title').text.split.join.gsub(re, "")
-  job.abilities = doc.search('table')[3].text.gsub(re, "")
-  @@all << self
-  @@all
+  @@all << {
+  :name => doc.search('h1.entry-title').text.split.join.gsub(re, ""),
+  :abilities => doc.search('table')[3].text.gsub(re, "")
+  } }
+  binding.pry
 
-end
 end
 end
