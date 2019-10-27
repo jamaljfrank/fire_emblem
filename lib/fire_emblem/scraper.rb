bcbb6a1aa7ac372f9307ab8e6b20dc42cd158d6f
Jamal Frank <jamaljfrank@gmail.com>
2019-10-27 05:04:49 +0000

    modified scaper

--- lib/fire_emblem/scraper.rb
+++ lib/fire_emblem/scraper.rb
@@ -1,7 +1,7 @@
 class FireEmblem::Scraper
   attr_accessor :job, :doc
   def initialize(job_name)
-    @job = Job.new 
+    @job = FireEmblem::Job.new 
     @job.name = job_name
     @doc = Nokogiri::HTML(open("https://samurai-gamers.com/fire-emblem-three-houses/#{job_name}-class/"))
     
