1d876f48b893e874ddd74b8fb9c8d3db95103e16
Jamal Frank <jamaljfrank@gmail.com>
2019-10-27 04:44:03 +0000

    modified fire_emblem.rb

--- lib/fire_emblem/scraper.rb
+++ lib/fire_emblem/scraper.rb
@@ -1,6 +1,21 @@
 class FireEmblem::Scraper
-  
+  attr_accessor :job, :doc
   def initialize(job_name)
     @job = Job.new 
     @job.name = job_name
     @doc = Nokogiri::HTML(open("https://samurai-gamers.com/fire-emblem-three-houses/#{job_name}-class/"))
+    
+  end
+  
+  def scrape 
+    scrape_details
+    @job  
+  end
+  
+  def scrape_details
+    words = ["Fire" , "Emblem:" , "Three" , "Houses" , "-" , "Class" , "Name" , "Effect" , "\n"]
+    re = Regexp.union(words)
+    @job.name = doc.search('h1.entry-title').text.split.join.gsub(re, "")
+    
+end
+end
