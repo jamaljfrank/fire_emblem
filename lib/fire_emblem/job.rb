ad49cb53eadcb81661f133760bc189cf0a6e6a4c
Jamal Frank <jamaljfrank@gmail.com>
2019-10-27 07:53:56 +0000

    modified scraper

--- lib/fire_emblem/job.rb
+++ lib/fire_emblem/job.rb
@@ -1,5 +1,5 @@
 class FireEmblem::Job 
- attr_accessor :name, :abilities
+ attr_accessor :name, :a1, :a2, :a3, :a4, :a5, :a1effect, :a2effect, :a3effect, :a4effect, :a5effect
  
   def self.all
   jobs = ['Archer','Armored-Knight','Assassin','Bishop','Bow-Knight','Brawler','Brigand','Cavalier','Dark-Bishop','Dark-Knight','Dark-Mage','Falcon-Knight','Fighter','Fortress-Knight','Grappler','Great-Knight','Gremory','Hero','Holy-Knight','Mage','Mercenary','Monk','Mortal-Savant','Myrmidon','Paladin','Pegasus-Knight','Priest','Sniper','Soldier','Swordmaster','Thief','Warlock','War-Master','Warrior','Wyvern-Lord','Wyvern-Rider']
@@ -9,6 +9,9 @@ class FireEmblem::Job
     @jobs = []
   end
   
+  def add_job(job)
+    @jobs << job
+  end
 end
 
 
--- lib/fire_emblem/scraper.rb
+++ lib/fire_emblem/scraper.rb
@@ -1,8 +1,18 @@
 class FireEmblem::Scraper
-  attr_accessor :job, :doc
+  attr_accessor :job, :doc 
   def initialize(job_name)
     @job = FireEmblem::Job.new 
     @job.name = job_name
+    @job.a1 = nil
+    @job.a2 = nil
+    @job.a3 = nil
+    @job.a4 = nil
+    @job.a5 = nil
+    @job.a1effect = nil
+    @job.a2effect = nil
+    @job.a3effect = nil
+    @job.a4effect = nil
+    @job.a5effect = nil
     @doc = Nokogiri::HTML(open("https://samurai-gamers.com/fire-emblem-three-houses/#{job_name}-class/"))
     
   end
@@ -12,10 +22,18 @@ class FireEmblem::Scraper
     @job  
   end
   
-  def scrape_details
+  def scrape_name
     words = ["Fire" , "Emblem:" , "Three" , "Houses" , "-" , "Class" , "Name" , "Effect" , "\n"]
     re = Regexp.union(words)
     @job.name = doc.search('h1.entry-title').text.split.join.gsub(re, "")
     
+  end
+  
+  def scrape_abilities
+    
+      @job.a1 = doc.search('table[3]:td[1]').text
+      
+  binding.pry
+  end
 end
-end
+
