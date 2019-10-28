3cb46f992e87c390d63bcaeeaf584338a4591a27
Jamal Frank <jamaljfrank@gmail.com>
2019-10-28 01:42:36 +0000

    modified scraper

--- lib/fire_emblem/scraper.rb
+++ lib/fire_emblem/scraper.rb
@@ -22,18 +22,29 @@ class FireEmblem::Scraper
     @job  
   end
   
-  def scrape_name
+  def scrape_all
     words = ["Fire" , "Emblem:" , "Three" , "Houses" , "-" , "Class" , "Name" , "Effect" , "\n"]
     re = Regexp.union(words)
     @job.name = doc.search('h1.entry-title').text.split.join.gsub(re, "")
-    
+    table = doc.search('table')[3]
+    @job.a1 = table.css('td')[0].text
+    @job.a2 = table.css('td')[2].text
+    @job.a3 = nil
+    @job.a4 = nil
+    @job.a5 = nil
+    @job.a1effect = table.css('td')[1].text
+    @job.a2effect = table.css('td')[3].text
+    @job.a3effect = nil
+    @job.a4effect = nil
+    @job.a5effect = nil
+    binding.pry
   end
   
   def scrape_abilities
     
       @job.a1 = doc.search('table[3]:td[1]').text
       
-  binding.pry
+  
   end
 end
 
