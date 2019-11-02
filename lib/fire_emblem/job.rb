2b5be4486110016ef385b4b704d0f8b8688d3d3c
Jamal Frank <jamaljfrank@gmail.com>
2019-11-02 21:07:44 +0000

    modified job

--- lib/fire_emblem/job.rb
+++ lib/fire_emblem/job.rb
@@ -4,13 +4,9 @@ class InvalidType < StandardError; end
 class FireEmblem::Job 
  attr_accessor :name, :a1, :a2, :a3, :a4, :a5, :a1effect, :a2effect, :a3effect, :a4effect, :a5effect
  
-  def self.all
-  jobs = ['Archer','Armored-Knight','Assassin','Bishop','Bow-Knight','Brawler','Brigand','Cavalier','Dark-Bishop','Dark-Knight','Dark-Mage','Falcon-Knight','Fighter','Fortress-Knight','Grappler','Great-Knight','Gremory','Hero','Holy-Knight','Mage','Mercenary','Monk','Mortal-Savant','Myrmidon','Paladin','Pegasus-Knight','Priest','Sniper','Soldier','Swordmaster','Thief','Warlock','War-Master','Warrior','Wyvern-Lord','Wyvern-Rider']
-  end
   
-  def initialize
-    @jobs = []
-  end
+  
+ 
   
   def add_job(job)
     if !job.is_a?(FireEmblem::Job)
