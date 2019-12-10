e26d79761986d224b33e8f29f56988dc57f64d3c
Jamal Frank <jamaljfrank@gmail.com>
2019-12-10 03:08:22 +0000

    modified cli

--- lib/fire_emblem/cli.rb
+++ lib/fire_emblem/cli.rb
@@ -13,6 +13,7 @@ puts title
 puts
 puts "Enter a class number for more information, Professor."
 puts
+puts
     list
     menu
     
@@ -20,7 +21,7 @@ puts
   
   def list 
     @jobs = ['Archer','Armored-Knight','Assassin','Bishop','Bow-Knight','Brawler','Brigand','Cavalier','Dark-Bishop','Dark-Knight','Dark-Mage','Falcon-Knight','Fighter','Fortress-Knight','Grappler','Great-Knight','Gremory','Hero','Holy-Knight','Mage','Mercenary','Monk','Mortal-Savant','Myrmidon','Paladin','Pegasus-Knight','Priest','Sniper','Soldier','Swordmaster','Thief','Warlock','War-Master','Warrior','Wyvern-Lord','Wyvern-Rider']
-    puts 
+    
  
     @jobs.each.with_index(1) do |j, i|
    puts "#{i}. #{j}" 
